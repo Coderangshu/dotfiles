b974735e2d63374a13becbdc2b77520f09118886
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2021-03-10 01:30:00 +0530

    fish config update by Garuda

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,40 @@
-﻿## Hide welcome message
+﻿## Set values
+# Hide welcome message
 set fish_greeting
 set VIRTUAL_ENV_DISABLE_PROMPT "1"
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-## Source .profile to apply its values
+
+# Set settings for https://github.com/franciscolourenco/done
+set -U __done_min_cmd_duration 10000
+set -U __done_notification_urgency_level low
+
+
+## Environment setup
+# Apply .profile
 source ~/.profile
 
-
-## Add ~/.local/bin to PATH
+# Add ~/.local/bin to PATH
 if test -d ~/.local/bin
     if not contains -- ~/.local/bin $PATH
         set -p PATH ~/.local/bin
     end
 end
 
+# Add depot_tools to PATH
+if test -d ~/Applications/depot_tools
+    if not contains -- ~/Applications/depot_tools $PATH
+        set -p PATH ~/Applications/depot_tools
+    end
+end
+
 
 ## Starship prompt
-source ("/usr/bin/starship" init fish --print-full-init | psub)
+if status --is-interactive
+   source ("/usr/bin/starship" init fish --print-full-init | psub)
+end
 
 
-## Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
+## Functions
+# Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
 function __history_previous_command
   switch (commandline -t)
   case "!"
@@ -46,8 +62,7 @@ else
   bind '$' __history_previous_command_arguments
 end
 
-
-## Fish command history
+# Fish command history
 function history
     builtin history --show-time='%F %T '
 end
@@ -56,8 +71,7 @@ function backup --argument filename
     cp $filename $filename.bak
 end
 
-
-## Copy DIR1 DIR2
+# Copy DIR1 DIR2
 function copy
     set count (count $argv | tr -d \n)
     if test "$count" = 2; and test -d "$argv[1]"
@@ -72,7 +86,7 @@ end
 
 ## Useful aliases
 # Replace ls with exa
-alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
+alias ls='exa --color=always --group-directories-first --icons' # preferred listing
 alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
 alias ll='exa -l --color=always --group-directories-first --icons'  # long format
 alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
@@ -114,18 +128,9 @@ alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pac
 # Help people new to Arch
 alias apt='man pacman'
 alias apt-get='man pacman'
+alias helpme='cht.sh --shell'
 alias please='sudo'
 alias tb='nc termbin.com 9999'
-alias paru="paru --bottomup"
-
-#Cleanup orphaned packages
-alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
-
-# Get the error messages from journalctl
-alias jctl="journalctl -p 3 -xb"
-
-# Recent installed packages
-alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 
 # Replace yay with paru
 [ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru'
